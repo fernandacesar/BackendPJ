8d3d0a576950db5bcc348df608d64d60bafee533
Fernanda Cesar <47528102+fernandacesar@users.noreply.github.com>
2022-10-29 12:44:25 -0300

    Criação de Routers

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import routes from "./routers";
 
 const app = express();
 
-app.use(morgan('dev')); 
+app.use(morgan("dev"));
 app.use(express.json());
-app.get('/teste', (request, response) => {
-    return response.json({message: "Hello World!"});
+
+app.use(routes);
+
+app.get("/teste", (request, response) => {
+  return response.json({ message: "Hello World!" });
 });
 
 app.listen(3333, () => {
   console.log(`Server started on port 3333!`);
-}); 
-
-function morgan(arg0: string): any {
-    throw new Error("Function not implemented.");
-}
-
+});
